1568e86c8f74171d281baa04cd20d69386091b60
Matia <matiasvillalobos84@gmail.com>
2024-11-20 01:40:56 -0300

    Antorchas y decoracion
    Se agaregaron las antorchas correspondientes para hacer el puzzle y se terminó de decorar

--- Scripts/cristales.gd
+++ Scripts/cristales.gd
@@ -3,13 +3,16 @@ extends Area2D
 @onready var text_label = $"/root/World/CanvasLayer/Texto Lore"
 @onready var image_rect = $"/root/World/CanvasLayer/Pergamino"
 
+var show_delay = 0.1 #mpo en segundos para mostrar los elementos
+var hide_delay = 0.2 # Tiempo en segundos para ocultar los elementos
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		#text_label.text = "¡Has encontrado un objeto!"
 		text_label.visible = true
 		image_rect.visible = true
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
+		await get_tree().create_timer(hide_delay).timeout
 		text_label.visible = false
 		image_rect.visible = false
