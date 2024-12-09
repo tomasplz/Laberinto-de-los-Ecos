60d53a947ec0285eec99100697b84adc0412a6de
Tomas Vargas <97796331+tomasplz@users.noreply.github.com>
2024-12-09 02:33:51 -0300

    Fix organizacion Desafios

--- .godot/mono/temp/obj/Debug/Laberinto de los Ecos.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Laberinto de los Ecos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laberinto de los Ecos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38a9cd23850026229161697240e00b55a7899ccb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c23f61f2ef99b7e9ae563235abc81f39100a7c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laberinto de los Ecos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laberinto de los Ecos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Scripts/area_2d.gd
+++ Scripts/area_2d.gd
@@ -1,27 +1,16 @@
 extends Area2D
-@onready var camera_main = $"../Camera2D"
-@onready var camera_secondary = $"../Camera2D2"
 
-var mission_completed = false  # Bandera para saber si la misión se completó
+var entered = false
+var scene_changed = false
 
+func _on_body_entered(body: PhysicsBody2D) -> void:
+	entered = true
 
+func _on_body_exited(body: Node2D) -> void:
+	entered = false
+	scene_changed = false
 
-func change_camera(to_secondary: bool):
-	# En Godot 4, asignar `current` directamente con un booleano no es posible
-	# En su lugar, se debe usar la función `Camera2D.make_current()`
-	
-	if to_secondary:
-		camera_secondary.make_current()  # Activa la cámara secundaria
-	else:
-		camera_main.make_current()
-# Llamado cuando la misión se complete
-func complete_mission():
-	mission_completed = true
-	change_camera(false)  # Vuelve a la cámara principal
-
-
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):  # Asegúrate de que el jugador tenga el grupo "player"
-		change_camera(true)
-	else:
-		print("Algo más entró en el área.")
+func _process(delta):
+	if entered and not scene_changed:
+		scene_changed = true
+		get_tree().change_scene_to_file("res://Scenes/world2.tscn")
