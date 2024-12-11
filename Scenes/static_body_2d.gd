29d757d942e88eb1de586ce3d388def397a81b7a
xwido10 <70038059+xwido10@users.noreply.github.com>
2024-12-11 19:55:37 -0300

    arreglo2.0

--- Scenes/static_body_2d.gd
+++ Scenes/static_body_2d.gd
@@ -5,10 +5,18 @@ extends Node
 @onready var camera2 =  $"../Camera2D2" # Referencia a la segunda cámara
 @onready var bloqueo=$"../StaticBody2D2"
 func _process(_delta):
-	# Detectar si se presionó la acción 'play_animation'
-	if Input.is_action_just_pressed("ui_animacioncopa"):
-		animation_player.play("copa")
-		bloqueo.queue_free();
-	if(Input.is_action_just_pressed("ui_cambio de camara")):
-		camera1.enabled=true  # Desactiva la primera cámara
-		camera2.enabled=false   # Activa la segunda cámara
+	if(camera2.enabled==true):# Detectar si se presionó la acción 'play_animation'
+		if Input.is_action_just_pressed("ui_animacioncopa"):
+			animation_player.play("copa")
+			if is_instance_valid(bloqueo):
+				bloqueo.queue_free()
+		if Input.is_action_just_pressed("mitad"):
+			animation_player.play("copamitad")
+			
+		if Input.is_action_just_pressed("vacia"):
+			animation_player.play("copacasivacia")
+			
+		if(Input.is_action_just_pressed("ui_cambio de camara")):
+			camera1.enabled=true  # Desactiva la primera cámara
+			camera2.enabled=false   # Activa la segunda cámara
+		
